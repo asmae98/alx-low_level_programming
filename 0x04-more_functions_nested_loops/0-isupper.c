c1d19045e0ba61b76556e0c602bb65f31cc7b84c
asmae98 <asmae.benabderrahmane@ynov.com>
2023-03-23 16:15:31 -0700

    commit loop

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int _isupper(int c)
+{
+    if (c >= 'A' && c <= 'Z') {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
